8a4cc0bb443a0b99ac532869f24ffe0ddf3d5d92
Byron Knoll <byron@byronknoll.com>
2015-11-11 21:07:58 -0800

    file templates

---
+++ battle-map-explorer.js
@@ -0,0 +1,8 @@
+/*
+battle-map-explorer version 1.0
+
+This code is released into the public domain - attribution is appreciated but not required.
+Made by Byron Knoll.
+
+https://github.com/byronknoll/battle-map-explorer
+*/
